c1a1876637c08cb42a6142d6fc58eda6d565dc44
Phong <pvo5@gmu.edu>
2020-10-27 18:36:58 -0400

    first commit

---
+++ src/main/java/com/deadlockArena/DeadlockArenaApplication.java
@@ -0,0 +1,13 @@
+package com.deadlockArena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeadlockArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeadlockArenaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/deadlockArena/config/package-info.java
@@ -0,0 +1,1 @@
+package com.deadlockArena.config;
